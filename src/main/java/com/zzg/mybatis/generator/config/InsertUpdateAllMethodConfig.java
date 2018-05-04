71944eb44f83d4992059f6d8c3e3e9eb7967047d
zengcai <zengcaiy@gmail.com>
2018-05-04 15:42:57 +0800

    插件化

---
+++ src/main/java/com/zzg/mybatis/generator/config/InsertUpdateAllMethodConfig.java
@@ -0,0 +1,112 @@
+package com.zzg.mybatis.generator.config;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.util.List;
+
+/**
+ * Created by zengcai on 2017/12/28.
+ */
+public class InsertUpdateAllMethodConfig extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean clientInsertMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientInsertMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean sqlMapInsertElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+//update
+
+    @Override
+    public boolean clientUpdateByPrimaryKeyWithBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientUpdateByPrimaryKeyWithBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientUpdateByPrimaryKeyWithoutBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientUpdateByPrimaryKeyWithoutBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeyWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeyWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+
+    //updateByExample
+
+
+    @Override
+    public boolean clientUpdateByExampleWithBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientUpdateByExampleWithBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientUpdateByExampleWithoutBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean clientUpdateByExampleWithoutBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByExampleWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean sqlMapUpdateByExampleWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean providerUpdateByExampleWithBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+
+    @Override
+    public boolean providerUpdateByExampleWithoutBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return false;
+    }
+}
---
+++ src/main/java/com/zzg/mybatis/generator/plugins/SelectOneByExamplePlugin.java
@@ -0,0 +1,83 @@
+package com.zzg.mybatis.generator.plugins;
+
+import com.itfsw.mybatis.generator.plugins.utils.BasePlugin;
+import com.itfsw.mybatis.generator.plugins.utils.FormatTools;
+import com.itfsw.mybatis.generator.plugins.utils.JavaElementGeneratorTools;
+import com.itfsw.mybatis.generator.plugins.utils.XmlElementGeneratorTools;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
+
+/**
+ * Created by zengcai on 2017/12/27.
+ */
+public class SelectOneByExamplePlugin extends BasePlugin {
+
+    public static final String METHOD_SELECT_ONE_BY_EXAMPLE = "selectOneByExample";  // 方法名
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        // 方法生成 selectOneByExample
+        Method method = JavaElementGeneratorTools.generateMethod(
+                METHOD_SELECT_ONE_BY_EXAMPLE,
+                JavaVisibility.DEFAULT,
+                JavaElementGeneratorTools.getModelTypeWithoutBLOBs(introspectedTable),
+                new Parameter(new FullyQualifiedJavaType(introspectedTable.getExampleType()), "example")
+        );
+        commentGenerator.addGeneralMethodComment(method, introspectedTable);
+
+        // interface 增加方法
+        FormatTools.addMethodWithBestPosition(interfaze, method);
+        logger.debug("查询单条数据插件:" + interfaze.getType().getShortName() + "增加selectOneByExample方法。");
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        // ------------------------------------ selectOneByExample ----------------------------------
+        // 生成查询语句
+        XmlElement selectOneElement = new XmlElement("select");
+        // 添加注释(!!!必须添加注释，overwrite覆盖生成时，@see XmlFileMergerJaxp.isGeneratedNode会去判断注释中是否存在OLD_ELEMENT_TAGS中的一点，例子：@mbg.generated)
+        commentGenerator.addComment(selectOneElement);
+
+        // 添加ID
+        selectOneElement.addAttribute(new Attribute("id", METHOD_SELECT_ONE_BY_EXAMPLE));
+        // 添加返回类型
+        selectOneElement.addAttribute(new Attribute("resultMap", introspectedTable.getBaseResultMapId()));
+        // 添加参数类型
+        selectOneElement.addAttribute(new Attribute("parameterType", introspectedTable.getExampleType()));
+        selectOneElement.addElement(new TextElement("select"));
+
+        StringBuilder sb = new StringBuilder();
+        if (stringHasValue(introspectedTable.getSelectByExampleQueryId())) {
+            sb.append('\'');
+            sb.append(introspectedTable.getSelectByExampleQueryId());
+            sb.append("' as QUERYID,");
+            selectOneElement.addElement(new TextElement(sb.toString()));
+        }
+        selectOneElement.addElement(XmlElementGeneratorTools.getBaseColumnListElement(introspectedTable));
+
+        sb.setLength(0);
+        sb.append("from ");
+        sb.append(introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime());
+        selectOneElement.addElement(new TextElement(sb.toString()));
+        selectOneElement.addElement(XmlElementGeneratorTools.getExampleIncludeElement(introspectedTable));
+
+        XmlElement ifElement = new XmlElement("if");
+        ifElement.addAttribute(new Attribute("test", "orderByClause != null"));  //$NON-NLS-2$
+        ifElement.addElement(new TextElement("order by ${orderByClause}"));
+        selectOneElement.addElement(ifElement);
+
+        // 只查询一条
+        selectOneElement.addElement(new TextElement("limit 1"));
+        // 添加到根节点
+        FormatTools.addElementWithBestPosition(document.getRootElement(), selectOneElement);
+        logger.debug("查询单条数据插件:" + introspectedTable.getMyBatis3XmlMapperFileName() + "增加selectOneByExample方法。");
+        return true;
+    }
+}
